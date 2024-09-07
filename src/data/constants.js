01bb23ca468b0e92eac188aabe0c31e1005ab096
Aakarsh Mishra <108521390+AakarshMishra@users.noreply.github.com>
2024-09-07 14:13:48 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/AakarshMishra",
   resume:
-    "https://drive.google.com/file/d/1KBQkUtzZ4Exnwl8yaci4xqwqtqVl6yFi/view?usp=sharing",
+    "https://drive.google.com/file/d/1fFjipmKE_9jdGIsCKXnw2wgKI5APN8_B/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/aakarsh-mishra07/",
   twitter: "",
   insta: "",
